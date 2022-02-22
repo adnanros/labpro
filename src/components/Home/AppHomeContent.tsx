f358c109add43c50bb36cf6b43a52e7e30398362
adnan <adnan.ros1@gmail.com>
2022-02-22 18:01:48 +0330

    sampleCategoryCard Completed

--- src/components/Home/AppHomeContent.tsx
+++ src/components/Home/AppHomeContent.tsx
@@ -4,6 +4,7 @@ import { CCol, CContainer, CRow, CSpinner } from '@coreui/react'
 
 // routes config
 import routes from '../../routes/routes'
+import SampleCategoryCard from './subcomponents/SampleCategoryCard';
 
 const AppHomeContent = React.memo(() => {
   return (
@@ -13,6 +14,20 @@ const AppHomeContent = React.memo(() => {
                 <img src="" alt="" />
             </CCol>
             <CCol lg={4} style={{backgroundColor:'blue'}}>3</CCol>
+        </CRow>
+        <CRow sm={{ cols: 2 }}>
+            <CCol>
+                <SampleCategoryCard />
+            </CCol>
+            <CCol >
+                <SampleCategoryCard />
+            </CCol>
+            <CCol >
+                <SampleCategoryCard />
+            </CCol>
+            <CCol >
+                <SampleCategoryCard />
+            </CCol>
         </CRow>
     </CContainer>
     )
--- src/components/Home/subcomponents/SampleCategoryCard.tsx
+++ src/components/Home/subcomponents/SampleCategoryCard.tsx
@@ -1,11 +1,25 @@
-import { CCol, CContainer, CRow, } from "@coreui/react";
+import { CButton, CCard, CCardBody, CCardImage, CCardText, CCardTitle, CCol, CContainer, CRow, } from "@coreui/react";
 import React from "react";
 import { Link } from "react-router-dom";
 
+var myImg = document.getElementById('myId') as HTMLImageElement;
+
+myImg?.setAttribute('src','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeu4wXOAa2ISH6oCkYHuJbcl6X4c1oJO1p6Q&usqp=CAU');
+
 const SampleCategoryCard = () => {
+    
     return (
         <div>
-
+            <CCard style={{ width: '18rem' }}>
+                <CCardImage id="myId" orientation="top" />
+                <CCardBody>
+                    <CCardTitle>Card title</CCardTitle>
+                    <CCardText>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </CCardText>
+                    <CButton href="#">Go somewhere</CButton>
+                </CCardBody>
+            </CCard>
         </div>
     );
 }
