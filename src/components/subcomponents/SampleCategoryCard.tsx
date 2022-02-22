45735ad58461e29c5c8075ae5cfef30600d80ae2
adnan <adnan.ros1@gmail.com>
2022-02-22 16:50:34 +0330

    sampleCategoryCard-init

---
+++ src/components/subcomponents/SampleCategoryCard.tsx
@@ -0,0 +1,13 @@
+import { CCol, CContainer, CRow, } from "@coreui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const SampleCategoryCard = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default SampleCategoryCard;
